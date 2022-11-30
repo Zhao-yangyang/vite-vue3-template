f6efaa0c1a913eb50889c2c763feba970774f99d
赵羊羊|15838645534 <yangyang.zhao@berlen.cn>
2022-11-30 11:59:54 +0800

    style: update

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,10 @@ export {};
 
 declare module "@vue/runtime-core" {
   export interface GlobalComponents {
+    ElButton: typeof import("element-plus/es")["ElButton"];
+    ElRow: typeof import("element-plus/es")["ElRow"];
+    IconEpAlarmClock: typeof import("~icons/ep/alarm-clock")["default"];
+    IconLogosVue: typeof import("~icons/logos/vue")["default"];
     RouterLink: typeof import("vue-router")["RouterLink"];
     RouterView: typeof import("vue-router")["RouterView"];
   }
