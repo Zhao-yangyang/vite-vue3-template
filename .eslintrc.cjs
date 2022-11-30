21a510fbb38b0f3fe47545df9d8dbf8dc4ba4f8c
赵羊羊|15838645534 <yangyang.zhao@berlen.cn>
2022-11-30 10:39:24 +0800

    style: prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,8 +11,8 @@ module.exports = {
   },
   root: true,
   extends: [
-    "plugin:vue/vue3-essential",
     "eslint:recommended",
+    "plugin:vue/vue3-essential",
     "@vue/eslint-config-typescript",
     "@vue/eslint-config-prettier",
   ],
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,6 +10,14 @@
     <el-button type="warning">Warning</el-button>
     <el-button type="danger">Danger</el-button>
   </el-row>
+  <div style="font-size: 20px">
+    <!-- 由于SVG图标默认不携带任何属性 -->
+    <!-- 你需要直接提供它们 -->
+    <Edit style="width: 1em; height: 1em; margin-right: 8px" />
+    <Share style="width: 1em; height: 1em; margin-right: 8px" />
+    <Delete style="width: 1em; height: 1em; margin-right: 8px" />
+    <Search style="width: 1em; height: 1em; margin-right: 8px" />
+  </div>
 </template>
 
 <style lang="scss" scoped>
